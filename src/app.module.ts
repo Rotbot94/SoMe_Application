dc03aa4b4f2dd4b4cf46a3c05f7cd8e766ac903f
Ali Issa <ali@dandesign.dk>
2023-02-15 11:25:29 +0100

    Added and setup TypeORM, added mysql, added mariadb to docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule} from "@nestjs/config";
+import { TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env'],
-    }),
+      }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    })
+
   ],
   controllers: [AppController],
   providers: [AppService],
