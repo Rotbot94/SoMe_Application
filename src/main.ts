f2bc00d247578f30b692fb196b73c8da567c02a3
Ali Issa <ali@dandesign.dk>
2023-02-21 18:58:48 +0100

    Console logging to debug server instance

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,10 @@ async function bootstrap() {
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 4000);
+  console.log(process.env.DB_PORT);
+  console.log(process.env.DB_HOST);
+  console.log(process.env.DB_USERNAME);
+  console.log(process.env.DB_PASSWORD);
+  console.log(process.env.DB_DATABASE);
 }
 bootstrap();
