054ea1cdc90e9bdf5d7b05cd03f7dafa8149abf6
Ali Issa <ali@dandesign.dk>
2023-02-24 13:21:01 +0100

    Added cors library

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,8 @@ import { useContainer } from 'class-validator';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 4000);
-  console.log(process.env.DB_PORT);
-  console.log(process.env.DB_HOST);
-  console.log(process.env.DB_USERNAME);
-  console.log(process.env.DB_PASSWORD);
-  console.log(process.env.DB_DATABASE);
 }
 bootstrap();
